0d001e7927d7803eaf2ff7e66c3a316b5e2a3ed5
Slowmagic <levitskiyorange@gmail.com>
2015-03-29 17:28:55 +0300

    Сделал вывод по ближайшим датам на главную страницу

--- WorkWithKOTE/Controllers/HomeController.cs
+++ WorkWithKOTE/Controllers/HomeController.cs
@@ -16,15 +16,54 @@ namespace WorkWithKOTE.Controllers
     {
         //TourContext db = new TourContext();
         UsersContext db = new UsersContext();
+        protected DateTime LowDate(string s,string k)
+        {
+            var data = db.Tour.Where(m => m.TypeOfTour == s && m.TourStatus == k).ToArray();
+            var date = new DateTime();
+            List<int> massId = new List<int>(); 
+            foreach(var item in data)
+            {
+               int j = item.TourId;
+               massId.Add(j);
+            }
+            List<DateTour> AllDateTour = new List<DateTour>();
+            foreach(int item in massId){
+                var DateTour = db.DateTours.Where(m => m.TourId == item).ToList();
+                AllDateTour.AddRange(DateTour);
+            }
+            for (int i = 0; i <AllDateTour.Count; i++)
+            {
+                var last = AllDateTour.Last();
+                if (AllDateTour[i].DateTourId != last.DateTourId)
+                {
+                    if (AllDateTour[i].FirstDate <= AllDateTour[i + 1].FirstDate && AllDateTour[i].FirstDate >= DateTime.Now)
+                    {
+                        if(AllDateTour[i].FirstDate <= date)
+                        date = AllDateTour[i].FirstDate;
+                    }
+                    else
+                        date = AllDateTour[i + 1].FirstDate;
+                }                 
+                else if (AllDateTour[i].FirstDate <= AllDateTour[0].FirstDate && AllDateTour[i].FirstDate >= DateTime.Now)
+                    date = AllDateTour[i].FirstDate;
+            }
+            return date;
+        }
         public ActionResult Index()
         {
             var data = new TourForHomePage();
-            data.TourHightPrev = db.Tour.Where(m => m.TypeOfTour == "Туры по Украине" && m.TourStatus == "Превью блока").FirstOrDefault();
-            data.TourCenterPrev = db.Tour.Where(m => m.TypeOfTour == "Туры в Грузию").FirstOrDefault(m => m.TourStatus == "Превью блока");
-            data.TourDownPrev = db.Tour.Where(m => m.TypeOfTour == "Приключенческие туры").FirstOrDefault(m => m.TourStatus == "Превью блока");
-            data.TourHight = db.Tour.Where(m => m.TourStatus == "Активный" && m.TypeOfTour == "Туры по Украине").ToList();
-            data.TourCenter = db.Tour.Where(m => m.TourStatus == "Активный" && m.TypeOfTour == "Туры в Грузию").ToList();
-            data.TourDown = db.Tour.Where(m => m.TourStatus == "Активный" && m.TypeOfTour == "Приключенческие туры").ToList();
+            var neeDate = LowDate("Туры по Украине", "Превью блока");
+            data.TourHightPrev = db.Tour.Where(m => m.DateTour.Any(k => k.FirstDate == neeDate) && m.TourStatus == "Превью блока" && m.TypeOfTour == "Туры по Украине").FirstOrDefault();
+            neeDate = LowDate("Туры в Грузию", "Превью блока");
+            data.TourCenterPrev = db.Tour.Where(m => m.DateTour.Any(k => k.FirstDate == neeDate) && m.TourStatus == "Превью блока" && m.TypeOfTour == "Туры в Грузию").FirstOrDefault();
+            neeDate = LowDate("Приключенческие туры", "Превью блока");
+            data.TourDownPrev = db.Tour.Where(m => m.DateTour.Any(k => k.FirstDate == neeDate) && m.TourStatus == "Превью блока" && m.TypeOfTour == "Приключенческие туры").FirstOrDefault();
+            neeDate = LowDate("Туры по Украине", "Активный");
+            data.TourHight = db.Tour.Where(m => m.DateTour.Any(k => k.FirstDate >= neeDate) && m.TourStatus == "Активный" && m.TypeOfTour == "Туры по Украине").ToList();
+            neeDate = LowDate("Туры в Грузию", "Активный");
+            data.TourCenter = db.Tour.Where(m => m.DateTour.Any(k => k.FirstDate >= neeDate) && m.TourStatus == "Активный" && m.TypeOfTour == "Туры в Грузию").ToList();
+            neeDate = LowDate("Приключенческие туры", "Активный");
+            data.TourDown = db.Tour.Where(m => m.DateTour.Any(k => k.FirstDate >= neeDate) && m.TourStatus == "Активный" && m.TypeOfTour == "Приключенческие туры").ToList();
             return View(data);
         }
         public ActionResult DateForCurrentTour(int id)
