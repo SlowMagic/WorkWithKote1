2070c4bcb1aaf27f38a185487b26648691f531a3
Slowmagic <levitskiyorange@gmail.com>
2015-04-04 13:45:41 +0300

    Production Version

---
+++ WorkWithKOTE/Controllers/AdminController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebMatrix.WebData;
+using WorkWithKOTE.Models;
+using System.Data;
+namespace WorkWithKOTE.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+        UsersContext db = new UsersContext();
+        [Authorize(Roles = "Admin")]
+        public ActionResult SearchAllUsers()
+        {
+            return View(db.UserProfiles.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult SearchAllUsers(string mail = null, string name = null, string surname = null, string age = null, string sex = null)
+        {
+            IQueryable<UserProfile> data = db.UserProfiles ;
+            if (!String.IsNullOrEmpty(mail))
+                data = data.Where(m => m.Email == mail);
+            if (!String.IsNullOrEmpty(name))
+                data = data.Where(m => m.RuFirstName == name);
+            if (!String.IsNullOrEmpty(surname))
+                data = data.Where(m => m.RuSecondName == surname);
+            if (!String.IsNullOrEmpty(age))
+            {
+                int bithdayYear = DateTime.Now.Year - int.Parse(age);
+                data = data.Where(m => m.Birthday.Value.Year == bithdayYear);
+            }
+            if (!String.IsNullOrEmpty(sex))
+                data = data.Where(m => m.Sex == sex);
+            return View(data);
+        }
+        public ActionResult RolesForUser(string moder)
+        {
+            var roles = (SimpleRoleProvider)Roles.Provider;
+            var membership = (SimpleMembershipProvider)Membership.Provider;
+            if (!roles.RoleExists("Moderator"))
+                roles.CreateRole("Moderator");
+            if (moder != null)
+            {
+                if (membership.GetUser(moder, false) == null)
+                {
+                    ViewBag.Message = "Такой почтовый ящик не зарегестрирован";
+                    return View();
+                }
+            }
+            if (!roles.IsUserInRole(moder, "Moderator"))
+                roles.AddUsersToRoles(new[] { moder }, new[] { "Moderator" });
+            return RedirectToAction("SearchAllUsers", "Admin");
+        }
+        public ActionResult BonusForUser(int? id, int? amtBonus)
+        {
+            var user = db.UserProfiles.Find(id);
+            if (user.Bonus != null)
+                user.Bonus = user.Bonus.Value + amtBonus;
+            else
+                user.Bonus = amtBonus;
+            db.Entry(user).State = EntityState.Modified;
+            db.SaveChanges();
+             return RedirectToAction("SearchAllUsers", "Admin");
+        }
+    }
+}
