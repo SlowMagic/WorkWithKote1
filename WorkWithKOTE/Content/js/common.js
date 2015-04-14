6d0b81233700a741db861ec6a708de88a90bf602
DencoDance <di.nekto@gmail.com>
2015-04-14 17:21:56 +0300

    profile and header design changes

--- WorkWithKOTE/Content/js/common.js
+++ WorkWithKOTE/Content/js/common.js
@@ -1,14 +1,31 @@
 /**
  * Created by DencoDance on 01.03.2015.
  */
+$(function () {
+     switchLogo(0);
+     $(window).scroll(function () {
+         var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+         switchLogo(scrollTop);
+     });
+     $(document).ready(function () {
+         $('a[href*=#]').bind("click", function (e) {
+             var anchor = $(this);
+             $('html, body').stop().animate({
+                 scrollTop: $(anchor.attr('href')).offset().top - 200
+             }, 1000);
+             e.preventDefault();
+         });
+         return false;
+     });
+ });
 function switchLogo(pos) {
     if ($('#big-logo').length == 0) {
         document.getElementById('small-logo').style.opacity = '1';
         $('#top-shadow').hide();
-        $('#user').attr('src', 'Content/img/user-black.png');
-        $('#Searching').attr('src', 'Content/img/Searching-black.png');
-        $('.separator').attr('src', 'Content/img/separator-black.png');
-        $('#small-logo').attr('src', 'Content/img/small-logo-red.png');
+        $('#user').attr('src', '/Content/img/user-black.png');
+        $('#Searching').attr('src', '/Content/img/Searching-black.png');
+        $('.separator').attr('src', '/Content/img/separator-black.png');
+        $('#small-logo').attr('src', '/Content/img/small-logo-red.png');
         document.getElementById('bar').style.backgroundColor = 'rgba(255,255,255, 1)';
         $('#username').addClass('username-other');
         $('nav').addClass('nav-other');
@@ -26,7 +43,7 @@ function switchLogo(pos) {
             document.getElementById('big-logo').style.opacity = '1';
             $('#top-shadow').show();
         }
-        if (document.getElementById('second-nav').getBoundingClientRect().top <= $('#bar').height()) {
+        if (document.getElementById('second-nav') && (document.getElementById('second-nav').getBoundingClientRect().top <= $('#bar').height())) {
             document.getElementById('second-nav').style.opacity = '0';
             document.getElementById('second-navigation').style.opacity = '1';
         }
